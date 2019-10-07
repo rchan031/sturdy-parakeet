f339d2df49788dc196584ae4fe6aa689d480b6ff
rchan031 <rchan031@ucr.edu>
2019-10-07 10:56:47 -0700

    Updating Lab3 Exercise 1

--- Lab3_bitManipulation/source/main.c
+++ Lab3_bitManipulation/source/main.c
@@ -14,10 +14,28 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;  //Configure port A's 8 pins as inputs
+    DDRB = 0x00; PORTB = 0xFF; //Configuring port B's 8 pins as inputs
+    
+    DDRC = 0xFF; PORTC = 0x00; //Configuring port c's 8 pins as outputs
 
     /* Insert your solution below */
     while (1) {
-
-    }
+		char counter = 0;
+		//char counter2 = 0;
+		char numBits = 0;
+		while(counter < 8) {
+			if((PINA & 0x80) == 0x80) {
+				PORTC = PORTC + 1;
+				PINA = PINA << 1;
+			}
+			if((PINB & 0x80) == 0x80) {
+				PORTC = PORTC + 1;
+				PINB = PINB << 1;
+			}
+			counter = counter + 1;
+		}
+		//PORTC = numBits;
+	}
     return 1;
 }
--- Lab3_bitManipulation/test/tests.gdb
+++ Lab3_bitManipulation/test/tests.gdb
@@ -26,19 +26,12 @@
 echo ======================================================\n
 echo Running all tests..."\n\n
 
-# Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
-# Set inputs
-setPINA 0x00
-setPINB 0x00
-# Continue for several ticks
+test "PINA: 0x5A, PINB: 0xA5, PORTC: 0x80"
+setPINA 0x5A
+setPINB 0xA5
 continue 2
-# Set expect values
-expectPORTC 0
-# Check pass/fail
+expectPORTC 8
 checkResult
-
-# Add tests below
 
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
